1191480c518691f5347ec510e5b077423c42ee66
muneebsultan525 <>
2024-03-29 10:40:39 +0500

    api linkage try 1

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -38,14 +38,16 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'api',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -127,3 +129,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': ('rest_framework.renderers.JSONRenderer',)
 }
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",  # React frontend URL
+]
--- Backend/Backend/utils.py
+++ Backend/Backend/utils.py
@@ -1,4 +1,6 @@
-from tensorflow.keras.preprocessing import image
+from tensorflow import keras
+from keras.preprocessing import image
+# from tensorflow.keras.preprocessing import image
 import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
@@ -15,7 +17,7 @@ def load_and_preprocess_image(img_path, target_size=(224, 224)):
 
 
 def display_image_and_prediction(model, image_path, class_names):
-    plt.figure(figsize=(5, 5))
+    # plt.figure(figsize=(5, 5))
 
     # Load and preprocess the image
     img_array = load_and_preprocess_image(image_path)  # You need to define load_and_preprocess_image function
@@ -26,14 +28,16 @@ def display_image_and_prediction(model, image_path, class_names):
     predicted_class_name = class_names[predicted_class]
 
     # Check if the prediction is correct or not
-    true_class_name = os.path.basename(os.path.dirname(image_path))
-    if true_class_name == predicted_class_name:
-        color = 'green'
-    else:
-        color = 'red'
+    # true_class_name = os.path.basename(os.path.dirname(image_path))
+    # if true_class_name == predicted_class_name:
+    #     color = 'green'
+    # else:
+    #     color = 'red'
 
-    img = mpimg.imread(image_path)
-    plt.imshow(img)
-    plt.title(f"Predicted: {predicted_class_name}", color=color)
-    plt.axis("off")
-    plt.show()
+    return predicted_class_name
+
+    # img = mpimg.imread(image_path)
+    # plt.imshow(img)
+    # plt.title(f"Predicted: {predicted_class_name}", color=color)
+    # plt.axis("off")
+    # plt.show()
